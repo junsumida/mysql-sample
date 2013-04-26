27acc6cf2292724b2239c58c325546812a9654d6
jun.sumida <jun.sumida@mixi.co.jp>
2013-04-26 21:15:43 +0900

    updated script

--- sample_generator.rb
+++ sample_generator.rb
@@ -2,8 +2,119 @@ require 'mysql2'
 
 client = Mysql2::Client.new(:host => "dev.mysql.lo.mixi.jp", :username => "root", :database => "jun_sumida")
 
-result = client.query("SELECT * FROM entry;");
+client.query("drop table if exists members")
+client.query("drop table if exists links")
+client.query("drop table if exists footprint_notify")
 
-result.each do |entry|
-    p entry
+client.query("
+    CREATE TABLE IF NOT EXISTS `members` (
+       id int unsigned not null auto_increment,
+        name varchar(255) not null,
+        sex enum('male', 'female', 'unknown'),
+        email varchar(255) not null,
+        primary key(`id`),
+        KEY idx_name(`name`)
+    ) Engine=InnoDB;
+")
+
+client.query("
+CREATE TABLE IF NOT EXISTS links (
+    from_id int unsigned not null,
+    to_id int unsigned not null,
+    created_at datetime not null,
+    primary key(`from_id`, `to_id`),
+    KEY idx_to_id(`to_id`)
+) Engine=InnoDB;
+")
+
+client.query("
+    CREATE TABLE IF NOT EXISTS footprint_notify (
+        member_id int unsigned not null,
+        created_at date not null,
+        unreads tinyint unsigned not null,
+        count int unsigned not null,
+        primary key(`member_id`, `created_at`),
+        KEY idx_date(`created_at`)
+    ) Engine=InnoDB;
+")
+
+
+def generate_str(length = 10)
+    words = ("a".."z").to_a + ("A".."Z").to_a 
+    length.times.map{words[rand(words.length + 1)]}.join 
+end
+
+def generate_sex
+    sex   = ['male', 'female', 'unknown']
+    sex[rand(3)] 
+end
+
+1.upto(1000).each do
+    client.query("
+        INSERT INTO members (
+            name,
+            sex,
+            email
+        ) VALUES (
+            '" + generate_str + "',
+            '" + generate_sex + "',
+            '" + generate_str + "@" + generate_str(5) + ".com'
+        );
+    ")
+       
+    #client.query("
+    #    INSERT INTO footprint_notify (
+    #        
+    #    )
+    #             
+    #")
+end
+
+1.upto(3000).each do
+    begin
+        from_id = rand(1000)
+        to_id   = rand(1000) 
+ 
+        if from_id != to_id 
+            to_id = rand(1000)
+        end
+
+        client.query("
+            INSERT INTO links (
+                from_id,
+                to_id,
+                created_at
+            ) VALUES (
+                " + from_id.to_s + ",
+                " + to_id.to_s   + ",
+                NOW()
+            );
+        ")
+    rescue
+        retry
+    else
+        client.query("
+            INSERT INTO links (
+                from_id,
+                to_id,
+                created_at
+            ) VALUES (
+                " + to_id.to_s + ",
+                " + from_id.to_s   + ",
+                NOW()
+            );
+        ")
+    end
+end
+
+#client.query("
+#    SELECT * FROM members;             
+#").each do |member|
+#    p member
+#end
+
+client.query("
+    SELECT * FROM links;
+").each do |link|
+    p link
 end
